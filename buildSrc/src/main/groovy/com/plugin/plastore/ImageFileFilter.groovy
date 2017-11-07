e1a6ff7ba59e96caeb4971d828fd0d83f74f15e4
Raul Guzman <rguzmanc.161@gmail.com>
2017-11-07 16:56:33 -0500

    add external library

---
+++ buildSrc/src/main/groovy/com/plugin/plastore/ImageFileFilter.groovy
@@ -0,0 +1,9 @@
+
+
+class ImageFileFilter implements FileFilter {
+    @Override
+    boolean accept(File pathname) {
+        return pathname.name.toLowerCase().endsWith('.png') ||
+                pathname.name.toLowerCase().endsWith('.jpg')
+    }
+}
