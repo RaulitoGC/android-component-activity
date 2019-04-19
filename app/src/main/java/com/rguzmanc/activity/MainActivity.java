aa1038b48a0eb2cd39a1e8bff9140340c112bc16
Raul Guzman <rguzmanc.161@gmail.com>
2019-04-18 21:16:08 -0500

    feat : update buttons and gradle

--- app/src/main/java/com/rguzmanc/activity/MainActivity.java
+++ app/src/main/java/com/rguzmanc/activity/MainActivity.java
@@ -3,40 +3,25 @@ package com.rguzmanc.activity;
 import android.app.Dialog;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
-import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
 import com.rguzmanc.activity.launchmode.DefaultActivity;
-import com.rguzmanc.activity.widget.SpinnerView;
 
 public class MainActivity extends AppCompatActivity {
 
     public static final String TAG = MainActivity.class.getSimpleName();
-
-    private Button mOpenAnotherActivityButton;
-    private Button mOpenDialogButton;
-    private Button mOpenProgressDialogButton;
-    private Button mOpenStateActivityButton;
-    private Button mOpenLaunchDemoButton;
     private ProgressDialog mProgressDialog;
     private Dialog mDialog;
-    private SpinnerView<String> mNamesSpinnerView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
-        mOpenAnotherActivityButton = (Button) findViewById(R.id.open_activity_button);
-        mOpenStateActivityButton = (Button) findViewById(R.id.open_state_activity_button);
-        mOpenProgressDialogButton = (Button) findViewById(R.id.open_progress_dialog_button);
-        mOpenDialogButton = (Button) findViewById(R.id.open_dialog_button);
-        mOpenLaunchDemoButton = (Button) findViewById(R.id.open_launch_demo_button);
-        mNamesSpinnerView = (SpinnerView) findViewById(R.id.open_name_list_spinner);
 
         mProgressDialog = new ProgressDialog(this);
         mProgressDialog.setMessage("Cargando...");
@@ -46,80 +31,67 @@ public class MainActivity extends AppCompatActivity {
         mDialog.setContentView(R.layout.dialog_custom);
         mDialog.setTitle("Title...");
 
-        mNamesSpinnerView.setEntries(new String[]{"Juan","Luis","Carlos","Raul","Cesar"});
-
-        mOpenAnotherActivityButton.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.open_activity_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToAnotherActivity();
             }
         });
 
-        mOpenLaunchDemoButton.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.open_launch_demo_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToLaunchDemoActivity();
             }
         });
 
-        mOpenStateActivityButton.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.open_state_activity_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 goToStateActivity();
             }
         });
 
-        mOpenProgressDialogButton.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.open_progress_dialog_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openProgressDialog();
             }
         });
 
-        mOpenDialogButton.setOnClickListener(new View.OnClickListener() {
+        findViewById(R.id.open_dialog_button).setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 openDialog();
             }
         });
 
-        mNamesSpinnerView.setOnItemSpinnerClick(new SpinnerView.OnSpinnerClick() {
-            @Override
-            public void onItemClick(int position) {
-                Log.d(TAG," spinner view on click");
-                Log.d(TAG,mNamesSpinnerView.getSelectedItem());
-            }
-
-            @Override
-            public void onRestoreItems() {
-
-            }
-        });
-
-        Log.d(TAG,"On create");
+        Log.d(TAG, "On create");
     }
 
-    private void goToAnotherActivity(){
-        startActivity(new Intent(this,DialogActivity.class));
+    private void goToAnotherActivity() {
+        startActivity(new Intent(this, DialogActivity.class));
     }
 
-    private void goToStateActivity(){
-        startActivity(new Intent(this,StateActivity.class));
+    private void goToStateActivity() {
+        startActivity(new Intent(this, StateActivity.class));
     }
 
-    private void goToLaunchDemoActivity(){startActivity(new Intent(this, DefaultActivity.class));}
+    private void goToLaunchDemoActivity() {
+        startActivity(new Intent(this, DefaultActivity.class));
+    }
 
-    private void openProgressDialog(){
+    private void openProgressDialog() {
         mProgressDialog.show();
         new Handler().postDelayed(new Runnable() {
             @Override
             public void run() {
                 mProgressDialog.dismiss();
             }
-        },30000);
+        }, 3000);
     }
 
-    private void openDialog(){
+    private void openDialog() {
         mDialog.show();
         Button dialogButton = (Button) mDialog.findViewById(R.id.dialogButtonOK);
         dialogButton.setOnClickListener(new View.OnClickListener() {
@@ -133,36 +105,36 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onStart() {
         super.onStart();
-        Log.d(TAG,"On start");
+        Log.d(TAG, "On start");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.d(TAG,"On resume");
+        Log.d(TAG, "On resume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.d(TAG,"On pause");
+        Log.d(TAG, "On pause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.d(TAG,"On stop");
+        Log.d(TAG, "On stop");
     }
 
     @Override
     protected void onRestart() {
         super.onRestart();
-        Log.d(TAG,"On restart");
+        Log.d(TAG, "On restart");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.d(TAG,"On destroy");
+        Log.d(TAG, "On destroy");
     }
 }
